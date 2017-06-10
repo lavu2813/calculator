8eaf96935e6f18f1d5025ccec3858430c3395ce4
Student 5 <student5@Felix-P-05.local>
2017-06-10 15:02:52 +0530

    Initial Commit

---
+++ CalculatorApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CalculatorApp
+//
+//  Created by Student 5 on 10/06/17.
+//  Copyright © 2017 Felix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CalculatorApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CalculatorApp
+//
+//  Created by Student 5 on 10/06/17.
+//  Copyright © 2017 Felix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
